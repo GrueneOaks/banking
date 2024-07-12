eaa7029ec66382531624deaaa601f8349c0314df
Chad Palmer <chad.n.palmer@protonmail.com>
2024-07-12 15:13:23 -0500

    added user actions

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,7 @@ import CustomInput from "./CustomInput"
 import { authFormSchema } from "@/lib/utils"
 import { Loader2 } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { signIn, signUp } from "@/lib/actions/user.actions"
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -46,16 +47,16 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create plaid token
 
       if(type === 'sign-up') {
-        // const newUser = await signUp(data);
-        // setUser(newUser);
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
 
       if(type === 'sign-in') {
-        // const response = await SignIn({
-        //   email: data.email,
-        //   password: data.password
-        // })
-        // if(response) router.push('/')
+        const response = await signIn({
+          email: data.email,
+          password: data.password
+        })
+        if(response) router.push('/')
       }
     } catch (error) {
       console.log(error);
